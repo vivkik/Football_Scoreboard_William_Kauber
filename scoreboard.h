9adc7b7aa7688e9d2648b89a4c4f414b21864a01
William Kauber <102266432+vivkik@users.noreply.github.com>
2022-04-11 03:50:03 +0000

    Added clock as variable in scoreboard class

--- scoreboard.h
+++ scoreboard.h
@@ -8,6 +8,7 @@ class Scoreboard
     int Quarter;
     int Down; 
     int ToGO;
+    double Clock;
     bool Pos; // Posession 
     Team Home; 
     Team Visitor; 
@@ -16,12 +17,14 @@ class Scoreboard
     void setQuarter(int);
     void setDown(int);
     void setToGo(int);
+    void setClock(double);
     void setPos(bool);
     void setHome(Team);
     void setVisitor(Team);
     int getQuarter();
     int getDown();
     int getToGo();
+    double getClock();
     bool getPos();
     Team getHome();
     Team getVisitor();
